9aa533496a1e4f80a9ef70391fe08a22b7a96452
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-12 17:40:47 +0530

    Update recursion3.go

--- recursion3.go
+++ recursion3.go
@@ -1,8 +1,10 @@
+//Recursion with base condition in Golang
 package main
 
 import "fmt"
 
 func print(n int) {
+	//base condition
 	if n > 0 {
 		fmt.Println("Good Morning! Ankit\n Good Night! Ankit ")
 		print(n - 1)
